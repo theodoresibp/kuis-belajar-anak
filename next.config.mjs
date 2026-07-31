30ad6ed0fb9ac1c7d30c3cb9d4204abfcfdbdfc0
Theo Bratta <96033636+theodoresibp@users.noreply.github.com>
2026-07-31 09:14:15 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "commons.wikimedia.org",
+        pathname: "/wiki/Special:FilePath/**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
